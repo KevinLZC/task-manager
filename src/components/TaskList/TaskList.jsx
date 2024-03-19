e7269b7c158a6ec799404ef69df5c743eb6b0926
KevinLZC <kevin_lazarillo@hotmail.com>
2024-03-19 08:39:29 -0600

    Adding counter for incompleted tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,10 +3,12 @@ import './TaskList.css';
 
 export function TaskList({ tasks, handleCompleted }) {
   let completedTasks = tasks.filter(task => task.completed).length
+  let incompleteTasks = tasks.filter(task => !task.completed).length
   return (
     <>
       <div className='task-container'>
         <h2>Tareas completadas: {completedTasks}</h2>
+        <h2>Tareas pendientes: {incompleteTasks}</h2>
         <hr />
         {tasks.map((task, index) => (
           <Task key={task.id} index={index} title={task.title} completed={task.completed} deadline={task.deadline} handleCompleted={handleCompleted} />
